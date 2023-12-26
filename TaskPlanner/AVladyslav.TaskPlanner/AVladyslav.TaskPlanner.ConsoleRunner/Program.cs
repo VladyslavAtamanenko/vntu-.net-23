594ea1d889a7f24a1685a1895fa00d97d4154372
Vladyslav Atamanenko <vladataman204@gmail.com>
2023-12-26 15:19:52 +0200

    Lab1 done!

--- TaskPlanner/AVladyslav.TaskPlanner/AVladyslav.TaskPlanner.ConsoleRunner/Program.cs
+++ TaskPlanner/AVladyslav.TaskPlanner/AVladyslav.TaskPlanner.ConsoleRunner/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AVladyslav.TaskPlanner.Domain.Logic;
+using AVladyslav.TaskPlanner.Domain.Models.enums;
+using AVladyslav.TaskPlanner.Domain.Models;
+
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Вiтаємо у Task Planner!");
+
+        // Accept user input for WorkItems
+        var workItems = GetWorkItemsFromUser();
+
+        // Create an instance of SimpleTaskPlanner and order the WorkItems
+        var planner = new SimpleTaskPlanner();
+        var orderedItems = planner.CreatePlan(workItems);
+
+        // Display the ordered WorkItems
+        Console.WriteLine("\nВпорядкованi справи:");
+        foreach (var item in orderedItems)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+
+    private static WorkItem[] GetWorkItemsFromUser()
+    {
+        Console.Write("\nВведiть кiлькiсть справ: ");
+        if (!int.TryParse(Console.ReadLine(), out int itemCount) || itemCount <= 0)
+        {
+            Console.WriteLine("Невалiдний ввiд. Вихiд з програми.");
+            Environment.Exit(1);
+        }
+
+        var workItems = new WorkItem[itemCount];
+
+        for (var i = 0; i < itemCount; i++)
+        {
+            Console.WriteLine($"\nСправа {i + 1}:");
+
+            Console.Write("Назва: ");
+            var title = Console.ReadLine();
+
+            Console.Write("Термiн виконання (dd.MM.yyyy): ");
+            if (!DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out var dueDate))
+            {
+                Console.WriteLine("Невалiдний формат дати. Вихiд з програми.");
+                Environment.Exit(1);
+            }
+
+            Console.Write("Прiорiтет (None, Low, Medium, High, Urgent): ");
+            if (!Enum.TryParse<Priority>(Console.ReadLine(), true, out var priority))
+            {
+                Console.WriteLine("Невалiдний прiорiтет. Вихiд з програми.");
+                Environment.Exit(1);
+            }
+
+            Console.Write("Складнiсть (None, Minutes, Hours, Days, Weeks): ");
+            if (!Enum.TryParse<Complexity>(Console.ReadLine(), true, out var complexity))
+            {
+                Console.WriteLine("Невалiдна cкладнiсть. Вихiд з програми.");
+                Environment.Exit(1);
+            }
+
+            workItems[i] = new WorkItem
+            {
+                Title = title,
+                DueDate = dueDate,
+                Priority = priority,
+                Complexity = complexity
+            };
+        }
+
+        return workItems;
+    }
+}
---
+++ TaskPlanner/AVladyslav.TaskPlanner/AVladyslav.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,29 @@
+﻿using AVladyslav.TaskPlanner.Domain.Models;
+
+namespace AVladyslav.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        public WorkItem[] CreatePlan(WorkItem[] items)
+        {
+            var itemsAsList = items.ToList();
+            itemsAsList.Sort(CompareWorkItems);
+            return itemsAsList.ToArray();
+        }
+        public static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
+        {
+            // Compare by Priority (descending)
+            int priorityComparison = secondItem.Priority.CompareTo(firstItem.Priority);
+            if (priorityComparison != 0)
+                return priorityComparison;
+
+            // Compare by DueDate (ascending)
+            int dueDateComparison = firstItem.DueDate.CompareTo(secondItem.DueDate);
+            if (dueDateComparison != 0)
+                return dueDateComparison;
+
+            // Compare by Title (alphabetical)
+            return string.Compare(firstItem.Title, secondItem.Title, StringComparison.Ordinal);
+        }
+    }
+}
